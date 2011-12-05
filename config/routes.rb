17ab3e0792938a135ebdd51a334c51c7321c6626
Diogo Castro <b.boy.souljah@gmail.com>
2011-12-05 10:14:29 +0000

    scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 CmovC2dmServer::Application.routes.draw do
-  get "properties/create"
+  resources :properties
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,6 +51,7 @@ CmovC2dmServer::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'properties#index'
 
   # See how all your routes lay out with "rake routes"
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111205093619) do
+ActiveRecord::Schema.define(:version => 20111205100856) do
 
   create_table "properties", :force => true do |t|
     t.string   "photo"
