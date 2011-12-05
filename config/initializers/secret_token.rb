c677f1b87d8ed78f557615d73445cdb5d25f8c72
Diogo Castro <b.boy.souljah@gmail.com>
2011-12-05 09:13:56 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CmovC2dmServer::Application.config.secret_token = 'b69cb7eebf63b1a2ccecd30b9194fac930066ae1b642942ec04445dc5dc26730ef018f216fc5202a404b3e924038a1e9d0bfe1af2a8c959eb692195112774000'
